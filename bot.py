27c039424b4f7a340c2b1fa4c22e2419d3e17424
Razor <razoroller529@gmail.com>
2021-06-01 20:12:18 -0300

    Removido discord.log, atualizado .gitignore
    Logs do terminal agora ficam em terminal.log e passou a ser um arquivo local de testes

--- bot.py
+++ bot.py
@@ -54,7 +54,7 @@ async def on_ready():
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(filename='terminal.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
