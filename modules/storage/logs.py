91ab68b9d3f55b5b43b978870964112f44528626
Razor <razoroller529@gmail.com>
2021-06-26 02:31:18 -0300

    Corrigir pequenos detalhes: adicionados comentários com os valores hexadecimais das cores em logs.py; atualizado o arquivo settings.py.example, que ainda não tinha configuração de módulos desativados; adicionada variável empty_value que permite deixar um campo de embed vazio.

--- modules/storage/logs.py
+++ modules/storage/logs.py
@@ -26,13 +26,13 @@ message_deleted_colour = 16711680 #ff0000
 member_join_colour = 61680 #00ffff
 
 # Valor decimal (da cor em hexadecimal) da cor da mensagem que registra saídas
-member_leave_colour = 16537091 #
+member_leave_colour = 16537091 #fc5603
 
 # Valor decimal (da cor em hexadecimal) da cor da mensagem que registra banimentos
 member_ban_colour = 16711680 #ff0000
 
 # Valor decimal (da cor em hexadecimal) da cor da mensagem que registra banimentos revogados
-member_unban_colour = 16537091 #
+member_unban_colour = 16537091 #fc5603
 
 # Valor decimal (da cor em hexadecimal) da cor da mensagem que registra convites criados
 invite_created_colour = 16776960 #ffff00
