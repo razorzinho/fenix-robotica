867efc9c1faceed4d52e419efd920446728ede46
Razor <razoroller529@gmail.com>
2021-07-11 14:22:51 -0300

    Remover widget de horário

--- modules/widgets.py
+++ modules/widgets.py
@@ -7,16 +7,6 @@ class Widgets(commands.Cog):
 
     def __init__(self, client):
         self.client = client
-        self.update_time_zone.start()
-
-    @tasks.loop(minutes=widgets.tasks_delay)
-    async def update_time_zone(self):
-        for guild in self.client.guilds:
-            guild = guild
-        now = datetime.localtime()
-        current_time = now.strftime('%H:%M')
-        channel = discord.utils.get(guild.voice_channels, id=widgets.time_zone_channel_id)
-        await channel.edit(name=f'Horário de Brasília: {current_time}', reason='Atualizar horário.')
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
