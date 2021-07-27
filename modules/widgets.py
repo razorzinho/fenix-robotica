4f4f9e171c58413e1ba8698c214199b98c7b2a62
r47orr <razoroller529@gmail.com>
2021-07-27 20:42:00 -0300

    Adicionar atualização de contagem de membros ao iniciar o bot

--- modules/widgets.py
+++ modules/widgets.py
@@ -9,6 +9,16 @@ class Widgets(commands.Cog):
         self.client = client
 
     @commands.Cog.listener()
+    async def on_ready(self):
+        print('Guida obtida.')
+        for guild in self.client.guilds:
+            guild = guild
+        count = guild.member_count
+        channel = discord.utils.get(guild.voice_channels, id=widgets.counting_channel_id)
+        print('Contagem de membros atualizada.')
+        await channel.edit(name=f'{count} membros', reason='Atualizar contagem de membros.')
+
+    @commands.Cog.listener()
     async def on_member_join(self, member):
         count = member.guild.member_count
         channel = discord.utils.get(member.guild.voice_channels, id=widgets.counting_channel_id)
