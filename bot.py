01c11632cb492d44084d9ef7b7a834bda97a70fe
r47orr <razoroller529@gmail.com>
2021-08-14 21:02:52 -0300

    Corrigir função quebrada de inicialização das partes adicionais do código principal; adicionar log de feedback em relação ao banco de dados

--- bot.py
+++ bot.py
@@ -1,22 +1,28 @@
 import discord
-import help
 from discord.ext import commands
 from data import settings
-intents = discord.Intents.all()
+from modules.storage import logs
+client = commands.Bot(command_prefix = settings.bot_prefix, intents=discord.Intents.all(), status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=settings.activity_name))
 
-client = commands.Bot(command_prefix = settings.bot_prefix, help_command=CustomHelpCommand, intents=intents, status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=settings.activity_name))
-client.load_extension('error_handler')
 client.load_extension('terminal')
-client.load_extension('disconnect')
+client.load_extension('error_handler')
 client.load_extension('extensions')
-client.load_extension('help')
-
+client.load_extension('nickname')
+#client.load_extension('help')
+client.load_extension('disconnect')
 @client.event
 async def on_ready():
-    if client.user.name is not f'[{settings.bot_prefix}] {settings.bot_name}':
-        await client.user.edit(username=f'[{settings.bot_prefix}] {settings.bot_name}')
-        print('Nome do bot atualizado.')
-    else:
-        print('Nome do bot já está correto.')
+    print('Inicializando banco de dados...')
+    try:
+        client.load_extension('database')
+        print('Banco de dados inicializado.')
+    except Exception as err:
+        print('Houve algum erro com o módulo do banco de dados. Registrando no canal de logs...')
+        logs_channel = client.get_channel(logs.bot_logs_channel_id)
+        embed = discord.Embed(colour=logs.bot_logout_colour)
+        embed.set_author(name=client.user.name, url=settings.url_panel, icon_url=client.user.avatar.url)
+        embed.add_field(name='O Banco de dados gerou o seguinte erro:', value=f'```{err}```', inline=False)
+        await logs_channel.send(embed=embed)
+        print('Erro do banco de dados registrado no canal de logs.')
 
 client.run(settings.bot_token)
