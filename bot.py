b8a3f677f792cddfad712b0cc90da2876705bcdd
r47orr <razoroller529@gmail.com>
2021-08-20 20:05:54 -0300

    Remover código do banco de dados do arquivo principal; corrigir erros impedindo conexão e troca de dados com o banco

--- bot.py
+++ bot.py
@@ -1,36 +1,14 @@
 import discord
-import database
 from discord.ext import commands
 from data import settings
-from modules.storage import logs
-client = commands.Bot(command_prefix = settings.bot_prefix, intents=discord.Intents.all(), status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=settings.activity_name))
+client = commands.Bot(command_prefix=settings.bot_prefix, intents=discord.Intents.all(), status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=settings.activity_name))
 
 # Carregar códigos essenciais do bot antes de carregar os móudlos
 client.load_extension('terminal')
 client.load_extension('error_handler')
 client.load_extension('extensions')
 client.load_extension('nickname')
-# Desabilitado por estar com desenvolvimento parado
-#client.load_extension('help')
+client.load_extension('help')
 client.load_extension('disconnect')
-
-# Inicialização do Banco de dados ocorre depois que o bot está on-line
-@client.event
-async def on_ready():
-    print('Inicializando banco de dados...')
-    # Tentar inicializar o banco de dados sem interromper o funcionamento do bot:
-    try:
-        client.load_extension('database')
-        print('Banco de dados carregado...')
-    # Se não for possível iniciar o banco de dados, registrar no canal de logs do servidor.
-    except Exception as err:
-        print('Houve algum erro com o módulo do banco de dados. Registrando no canal de logs...')
-        logs_channel = client.get_channel(logs.bot_logs_channel_id)
-        embed = discord.Embed(colour=logs.bot_logout_colour)
-        embed.set_author(name=client.user.name, url=settings.url_panel, icon_url=client.user.avatar.url)
-        embed.add_field(name='O módulo do banco de dados gerou o seguinte erro:', value=f'```{err}```', inline=False)
-        await logs_channel.send(embed=embed)
-        print('Erro do banco de dados registrado no canal de logs.')
-    database.get_database(settings.db)
 
 client.run(settings.bot_token)
--- database.py
+++ database.py
@@ -1,23 +1,24 @@
+import discord
 import pymongo
 from data import settings
-from modules.storage import *
+from modules.storage import logs
 
 # Função de conexão ao banco de dados:
 def get_database(db):
     if not db:
         raise TypeError('O banco de dados que será utilizado deve ser especificado.')
     # Linha que define a conexão ao banco usando os dados:
-    CONNECTION_STRING = f'{settings.host}:{settings.password}@{settings.cluster}.{settings.port}/{db}?retryWrites=true&w=majority'
+    connection_string = f'{settings.host}{settings.user}:{settings.password}@{settings.cluster}.{settings.port}/{db}?retryWrites=true&w=majority'
     # Iniciar conexão
     try:
-        cursor = pymongo.MongoClient(CONNECTION_STRING)
+        cursor = pymongo.MongoClient(connection_string)
         # Criar o banco de dados:
         print('Banco de dados acessado com êxito.')
         return cursor[db]
     except:
         raise TypeError('Não foi possível conectar-se ao banco de dados, ou algum dado de conexão está incorreto.')
 
-# Função de registro de dados na coleção desejada:
+# Função de registro de dados na coleção especificada:
 def to_collection(db:str, collection:str, data):
     if not db:
         raise TypeError('O banco de dados deve ser especificado.')
@@ -27,7 +28,7 @@ def to_collection(db:str, collection:str, data):
         raise TypeError('Algum dado deve ser passado para ser inserido ao banco.')
     get_db = get_database(db)
     collection = get_db[collection]
-    collection.insert_once(data)
+    collection.insert_one(data)
 
 # Função de obtenção de dados do banco de dados:
 def from_database(db, collection, data=None, one: bool=False):
@@ -37,19 +38,27 @@ def from_database(db, collection, data=None, one: bool=False):
     db = get_database(db)
     collection = db[collection]
     if one:
-        return collection.find_one(data)
-    return collection.find({}, data)
+        item = collection.find_one(data)
+        return item
+        #for item in items:
+        #    return item
+    elif data:
+        items = collection.find({}, data)
+        for item in items:
+            return item
+    items = collection.find()
+    for item in items:
+        return item
 
 def test_db():
         try:
-            dbname = get_database(settings.db)
+            col = 'testing'
             test = {
                 'data':
                 {
                     'Foo': 'Success'
                 }
             }
-            col = 'testing'
             to_collection(db=settings.db, collection=col, data=test)
             print(test)
             print('OK! Dados acima enviados ao banco de dados.')
@@ -57,20 +66,18 @@ def test_db():
         except:
             raise TypeError('O teste de registro falhou por algum motivo.')
         try:
-            dbname = from_database(settings.db, collection='test', one=False)
+            dbname = from_database(settings.db, collection='testing')
             print(dbname)
             print('Teste de extração de dados realizado com êxito.')
         except:
             raise TypeError('Algo deu errado com o teste de extração de dados.')
+        print(get_database('Coleções existentes no banco:\n' + '''settings.db).list_collection_names()'''))
 
-
-# Isto faz com que seja possível que outros arquivos do prorama utilizem esta conexão ao banco:
-if __name__ == "__main__":
-    test_db()
-
+# Início das rotinas que o bot executa
 def setup(client):
+
+# Inicialização do Banco de dados ocorre depois que o bot está on-line
     @client.event
     async def on_ready():
-        print('Banco de dados executado...')
-        get_database(settings.db)
+        print('Inicializando banco de dados...')
         test_db()
