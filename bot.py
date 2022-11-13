40c8038a26de96d3b8dcbbde9bedd7c428dff3dc
r47orr <razoroller529@gmail.com>
2022-11-13 19:55:37 -0300

    Create reaction-roles branch

--- bot.py
+++ bot.py
@@ -1,16 +1,30 @@
 import discord
-from discord.ext import commands
+from discord import app_commands
 from data import settings
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
 # Definição do bot e seus parâmetros principais:
 client = commands.Bot(command_prefix=settings.bot_prefix, intents=discord.Intents.all(), status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=settings.activity_name))
 
+try:
+    synced = await bot.tree.sync()
+    print(f"{len(synced)} comandos sincronizados.")
+except  Exception as e:
+    print(e)
+
+@bot.tree.command(name="ping")
+async def ping(interaction: discord.Interaction):
+    interaction.response.send_message(f"Pong! Tempo de resposta atual: `{interaction.client.latency}`")
+
 # Carregar códigos essenciais do bot antes de carregar os móudlos
-client.load_extension('terminal')
-client.load_extension('error_handler')
-client.load_extension('extensions')
-client.load_extension('nickname')
-client.load_extension('help')
-client.load_extension('disconnect')
+#client.load_extension('terminal')
+#client.load_extension('error_handler')
+#client.load_extension('extensions')
+#client.load_extension('nickname')
+#client.load_extension('help')
+#client.load_extension('disconnect')
 
 # Inicializar o bot. Token no arquivo /data/settings.py
-client.run(settings.bot_token)
+client.run(config.CLIENT_TOKEN)
--- modules/storage/help.py
+++
@@ -1,7 +0,0 @@
-help_colour = 16776960 #ffff00
-
-cog_help_colour = 16776960 #ffff00
-
-group_help_colour = 16537091 #fc5603
-
-command_help_colour = 16711680 #ff0000
